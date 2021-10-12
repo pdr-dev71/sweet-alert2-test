4b12257f76a2e8f9dfc555dae4d0728e30dcaa6f
pdr-dev71 <joaopedrosr2699@gmail.com>
2021-10-12 18:40:02 -0300

    Added alerts using sweetalert2

---
+++ helloword/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div id="app" class="container mt-3">
+      <b-button @click="showAlert" class="mr-2">Login</b-button>
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <b-button @click="signup" class="mr-4">Cadastrar</b-button>
+    </div>
+    <b-card
+    no-body
+    style="max-width: 20rem;"
+    img-src="https://placekitten.com/380/200"
+    img-alt="Image"
+    img-top
+  >
+    <template #header>
+      <h4 class="mb-0">Hello World</h4>
+    </template>
+
+    <b-card-body>
+      <b-card-title>Card Title</b-card-title>
+      <b-card-sub-title class="mb-2">Card Sub Title</b-card-sub-title>
+      <b-card-text>
+        Some quick example text to build on the card title and make up the bulk of the card's
+        content.
+      </b-card-text>
+    </b-card-body>
+
+    <b-list-group flush>
+      <b-list-group-item>Cras justo odio</b-list-group-item>
+      <b-list-group-item>Dapibus ac facilisis in</b-list-group-item>
+      <b-list-group-item>Vestibulum at eros</b-list-group-item>
+    </b-list-group>
+
+    <b-card-body>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </b-card-body>
+
+    <b-card-footer>This is a footer</b-card-footer>
+
+    <b-card-img src="https://placekitten.com/480/210" alt="Image" bottom></b-card-img>
+  </b-card>
+  </div> 
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    showAlert() {   
+      this.$swal({
+        title: 'Atenção!',
+        text: 'Aconteceu um erro na sua requisição, tente novamente mais tarde',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      })
+    },
+    signup() {
+      this.$swal({
+        title: 'Finalizado',
+        text: 'Cadastro no Super Hentais comcluído com sucesso',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      })
+      
+    }
+  } 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ helloword/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueSweetalert2 from "vue-sweetalert2";
+
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import "sweetalert2/dist/sweetalert2.min.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueSweetalert2);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
